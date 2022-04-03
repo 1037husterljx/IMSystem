bdb4f215d67983f9ba416205dd7041baeb48ed50
1037husterljx <sos11088@outlook.com>
2022-04-03 13:32:45 +0800

    用户消息广播

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -34,6 +35,25 @@ func (this *Server) handle(connect net.Conn) {
 	//广播上线
 	this.BroadCast(user, "已上线")
 
+	go func() {
+		buf := make([]byte, 4096)
+
+		n, err := connect.Read(buf)
+
+		if n == 0 {
+			this.BroadCast(user, "已下线")
+		}
+
+		if err != nil && err != io.EOF {
+			fmt.Println("connect Read err:", err)
+			return
+		}
+		//去掉\n
+		msg := buf[:n-1]
+
+		this.BroadCast(user, string(msg))
+	}()
+
 	select {}
 }
 
@@ -45,7 +65,7 @@ func (this *Server) BroadCast(user *User, msg string) {
 func (this *Server) ListenMessage() {
 	for {
 		msg := <-this.Channle
-		
+
 		this.Lock.Lock()
 		for _, user := range this.Map {
 			user.Channle <- msg
@@ -56,7 +76,7 @@ func (this *Server) ListenMessage() {
 
 func (this *Server) Start() {
 	listen, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
-	
+
 	if err != nil {
 		fmt.Println("listen err:", err)
 		return
@@ -65,10 +85,10 @@ func (this *Server) Start() {
 	defer listen.Close()
 
 	go this.ListenMessage()
-	
+
 	for {
 		connect, err := listen.Accept()
-		
+
 		if err != nil {
 			fmt.Println("accept err:", err)
 			continue
